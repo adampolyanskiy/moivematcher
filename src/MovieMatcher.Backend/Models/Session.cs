06f86c55de1a0bb787f0260c53e7f9906a93f67c
adam <adampolyanskiy@gmail.com>
2025-03-02 14:35:32 +0400

    make session thread-safe

--- src/MovieMatcher.Backend/Models/Session.cs
+++ src/MovieMatcher.Backend/Models/Session.cs
@@ -1,11 +1,82 @@
-﻿namespace MovieMatcher.Backend.Models;
+﻿using System.Collections.Concurrent;
+
+namespace MovieMatcher.Backend.Models;
 
 public class Session
 {
     public required string Id { get; init; }
-    public HashSet<string> ConnectionIds { get; init; } = [];
-    public Queue<Movie> Movies { get; set; } = new();
     public required SessionOptions Options { get; init; }
-    public Dictionary<string, HashSet<string>> MovieLikes { get; init; } = new();
-    public List<Match> Matches { get; set; } = [];
+
+    public int ConnectionCount => _connectionIds.Count;
+
+    private readonly ConcurrentDictionary<string, byte> _connectionIds = new();
+    private readonly ConcurrentDictionary<string, ConcurrentQueue<Movie>> _userMovieQueues = new();
+    private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, byte>> _movieLikes = new();
+    private readonly ConcurrentDictionary<string, Match> _matches = new();
+
+    public bool AddConnection(string connectionId)
+    {
+        if (!_connectionIds.TryAdd(connectionId, 0))
+        {
+            return false;
+        }
+
+        _userMovieQueues.TryAdd(connectionId, new ConcurrentQueue<Movie>());
+        return true;
+    }
+
+    public bool RemoveConnection(string connectionId)
+    {
+        if (!_connectionIds.TryRemove(connectionId, out _))
+        {
+            return false;
+        }
+
+        _userMovieQueues.TryRemove(connectionId, out _);
+        return true;
+    }
+    
+    public bool ContainsConnection(string connectionId) => _connectionIds.ContainsKey(connectionId);
+
+    public void EnqueueMovie(Movie movie)
+    {
+        foreach (var queue in _userMovieQueues.Values)
+        {
+            queue.Enqueue(movie);
+        }
+    }
+
+    public bool TryDequeueMovie(string connectionId, out Movie? movie)
+    {
+        movie = null;
+
+        if (_userMovieQueues.TryGetValue(connectionId, out var queue))
+        {
+            return queue.TryDequeue(out movie);
+        }
+
+        return false;
+    }
+
+    public void AddMovieLike(string movieId, string userId)
+    {
+        var userSet = _movieLikes.GetOrAdd(movieId, _ => new ConcurrentDictionary<string, byte>());
+        if (!userSet.TryAdd(userId, 0))
+        {
+            return;
+        }
+
+        if (userSet.Count == _connectionIds.Count && !_connectionIds.IsEmpty)
+        {
+            _matches.TryAdd(movieId, new Match
+            {
+                MovieId = movieId,
+            });
+        }
+    }
+    
+    public bool MatchExists(string movieId)
+    {
+        return _matches.ContainsKey(movieId);
+    }
 }
