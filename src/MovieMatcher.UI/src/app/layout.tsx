f91e0b90a7425296d017e590c4b442230d67f0ee
adam <adampolyanskiy@gmail.com>
2025-03-03 13:30:51 +0400

    install tailwind

--- src/MovieMatcher.UI/src/app/layout.tsx
+++ src/MovieMatcher.UI/src/app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
+import "./styles/global.css";
 
 export const metadata: Metadata = {
   title: "Movie Matcher",
   description: "Find movies with your friends that you'll both love",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
--- src/MovieMatcher.UI/src/app/page.tsx
+++ src/MovieMatcher.UI/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Movie Matcher</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to Movie Matcher</h1>
     </div>
   );
 }
