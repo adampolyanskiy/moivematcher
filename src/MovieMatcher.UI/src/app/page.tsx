5d0d769dd5c83eeadcfaca7cdc47b413f0f40940
adam <adampolyanskiy@gmail.com>
2025-03-03 14:23:57 +0400

    add basic layout

--- src/MovieMatcher.UI/src/app/page.tsx
+++ src/MovieMatcher.UI/src/app/page.tsx
@@ -1,7 +1,46 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Welcome to Movie Matcher</h1>
+    <div className="flex flex-col items-center justify-start min-h-screen pt-[25vh] text-center">
+      <header className="text-center">
+        <h1 className="text-6xl font-extrabold tracking-tight">
+          🎬 MovieMatcher
+        </h1>
+        <h2 className="text-xl md:text-2xl text-gray-600 mt-3 max-w-2xl mx-auto">
+          Swipe through movies, match with friends, and discover the perfect
+          film to watch together!
+        </h2>
+      </header>
+      <div className="flex justify-center">
+        {/* Invite Button */}
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="mt-4">Create New Game</Button>
+          </DialogTrigger>
+
+          {/* Popup Modal */}
+          <DialogContent>
+            <DialogTitle>Start a New Game</DialogTitle>
+
+            {/* Form Fields */}
+            <div className="space-y-4">Game Options</div>
+
+            {/* Action Buttons */}
+            <DialogFooter>
+              <Button variant="secondary">Cancel</Button>
+              <Button variant="default">Start</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
   );
 }
